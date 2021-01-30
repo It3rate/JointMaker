c1365c5811d08b1b914ac03689aa2e89e686044c
Robin Debreuil <debreuil.robin@gmail.com>
2021-01-30 17:10:24 -0600

    More encoding work.

--- SketchEncoder.py
+++ SketchEncoder.py
@@ -39,7 +39,7 @@ class SketchEncoder:
         self.lineKeys = []
         self.lineValues = []
         self.chains = []
-        self.constraints = []
+        self.constraints = {}
         self.dimensions = {}
         self.encodeFromSketch()
 
@@ -63,6 +63,8 @@ class SketchEncoder:
                 print(comma + self.printLineIndexes(line), end="")
                 comma = ","
 
+        print("\nconstraints:")
+        self.encodeAllConstraints()
         print("\ndimensions:")
         self.encodeAllDimensions()
         
@@ -80,6 +82,11 @@ class SketchEncoder:
             if not line.entityToken in tokens:
                 chains.append(self.appendConnectedCurves(line, tokens))
         return chains
+
+    def encodeAllConstraints(self):
+        dims = []
+        for con in self.sketch.geometricConstraints:
+            self.constraints[con.entityToken] = self.encodeConstraint(con)
 
     def encodeAllDimensions(self):
         dims = []
@@ -110,15 +117,76 @@ class SketchEncoder:
     def linePointIndexes(self, line:f.SketchLine):
         return [self.pointIndex(line.startSketchPoint.entityToken), self.pointIndex(line.endSketchPoint.entityToken)]
 
+    def encodeConstraint(self, con:f.GeometricConstraint):
+        result = ""
+        cType = type(con)
+        if(cType is f.VerticalConstraint or cType is f.HorizontalConstraint):
+            result = "VH" + self.encodeEntity(con.line)
+        elif(cType is f.ParallelConstraint):
+            cCon:f.ParallelConstraint = con
+            result = "PA" + self.encodeEntity(cCon.lineOne) + self.encodeEntity(cCon.lineTwo)
+        elif(cType is f.PerpendicularConstraint):
+            cCon:f.PerpendicularConstraint = con
+            result = "PE" + self.encodeEntity(cCon.lineOne) + self.encodeEntity(cCon.lineTwo)
+        elif(cType is f.EqualConstraint):
+            cCon:f.EqualConstraint = con
+            result = "EQ" + self.encodeEntity(cCon.curveOne) + self.encodeEntity(cCon.curveTwo)
+        elif(cType is f.CollinearConstraint):
+            cCon:f.CollinearConstraint = con
+            result = "CO" + self.encodeEntity(cCon.lineOne) + self.encodeEntity(cCon.lineTwo)
+        elif(cType is f.SymmetryConstraint):
+            cCon:f.SymmetryConstraint = con
+            result = "SY" + self.encodeEntity(cCon.entityOne) + self.encodeEntity(cCon.entityTwo) + self.encodeEntity(cCon.symmetryLine)
+        elif(cType is f.MidPointConstraint):
+            cCon:f.MidPointConstraint = con
+            result = "MI" + self.encodeEntity(cCon.midPointCurve) + self.encodeEntity(cCon.point)
+            
+        print(result)
+        return result
+
+
     def encodeDimension(self, dim:f.SketchDimension):
         result = ""
         if(type(dim) == f.SketchLinearDimension):
             tdim:f.SketchLinearDimension = dim
-            result = "SLD" + self.encodeEntity(tdim.entityOne) + self.encodeEntity(tdim.entityTwo) + self.encodeExpression(tdim.parameter)
-              # all points, lines etc need to be stored and indexed in one array?, as len can be point to line. Have points first, and a 'lastPointIndex' if needed. 
+            result = "SLD" + self.encodeEntity(tdim.entityOne) + self.encodeEntity(tdim.entityOne) + self.encodeExpression(tdim.parameter)
+
         elif(type(dim) == f.SketchOffsetDimension):
             tdim:f.SketchOffsetDimension = dim
-            result = "SOD" + self.encodeEntity(tdim.line) + self.encodeEntity(tdim.entityTwo) + self.encodeExpression(tdim.parameter)
+            result = "SOD" + self.encodeEntity(tdim.line) + self.encodeExpression(tdim.parameter)
+
+        elif(type(dim) == f.SketchAngularDimension):
+            tdim:f.SketchAngularDimension = dim
+            result = "SAD" + self.encodeEntity(tdim.lineOne) + self.encodeEntity(tdim.lineTwo) + self.encodeExpression(tdim.parameter)
+
+        elif(type(dim) == f.SketchDiameterDimension):
+            tdim:f.SketchDiameterDimension = dim
+            result = "SDD" + self.encodeEntity(tdim.entity) + self.encodeExpression(tdim.parameter)
+
+        elif(type(dim) == f.SketchRadialDimension):
+            tdim:f.SketchRadialDimension = dim
+            result = "SRD" + self.encodeEntity(tdim.entity) + self.encodeExpression(tdim.parameter)
+
+        elif(type(dim) == f.SketchEllipseMajorRadiusDimension):
+            tdim:f.SketchEllipseMajorRadiusDimension = dim
+            result = "SMA" + self.encodeEntity(tdim.ellipse) + self.encodeExpression(tdim.parameter)
+
+        elif(type(dim) == f.SketchEllipseMinorRadiusDimension):
+            tdim:f.SketchEllipseMinorRadiusDimension = dim
+            result = "SMI" + self.encodeEntity(tdim.ellipse) + self.encodeExpression(tdim.parameter)
+
+        elif(type(dim) == f.SketchConcentricCircleDimension):
+            tdim:f.SketchConcentricCircleDimension = dim
+            result = "SCC" + self.encodeEntity(tdim.circleOne) +self.encodeEntity(tdim.circleTwo) + self.encodeExpression(tdim.parameter)
+
+            # def addDistanceDimension(self, pointOne, pointTwo, orientation, textPoint, isDriving):
+            # def addOffsetDimension(self, line, entityTwo, textPoint, isDriving): # also SketchOffsetCurvesDimension
+            # def addAngularDimension(self, lineOne, lineTwo, textPoint, isDriving):
+            # def addDiameterDimension(self, entity, textPoint, isDriving):
+            # def addRadialDimension(self, entity, textPoint, isDriving):
+            # def addEllipseMajorRadiusDimension(self, ellipse, textPoint, isDriving):
+            # def addEllipseMinorRadiusDimension(self, ellipse, textPoint, isDriving):
+            # def addConcentricCircleDimension(self, circleOne, circleTwo, textPoint, isDriving):
 
         print(result)
         return result
@@ -138,4 +206,8 @@ class SketchEncoder:
         return result
 
     def printLineIndexes(self, line:f.SketchLine):
-        return "(" + str(self.pointIndex(line.startSketchPoint.entityToken)) + "," + str(self.pointIndex(line.endSketchPoint.entityToken)) + ")"
+        if type(line) is f.SketchLine:
+            return "(" + str(self.pointIndex(line.startSketchPoint.entityToken)) + "," + str(self.pointIndex(line.endSketchPoint.entityToken)) + ")"
+        elif type(line) is f.SketchCircle:
+            circ:f.SketchCircle = line
+            return "(" + str(self.pointIndex(circ.centerSketchPoint.entityToken)) + ")"
